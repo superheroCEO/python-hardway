39f45e9f9600ffb2a7ff2fe27e2b18b91af29938
superheroCEO <ja@bringbackfit.com>
2016-06-21 13:59:52 -0700

    adding exercise 30

---
+++ ex30.py
@@ -0,0 +1,47 @@
+ 1
+ 2
+ 3
+ 4
+ 5
+ 6
+ 7
+ 8
+ 9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+people = 30
+cars = 40
+trucks = 15
+
+
+if cars > people:
+    print "We should take the cars."
+elif cars < people:
+    print "We should not take the cars."
+else:
+    print "We can't decide."
+
+if trucks > cars:
+    print "That's too many trucks."
+elif trucks < cars:
+    print "Maybe we could take the trucks."
+else:
+    print "We still can't decide."
+
+if people > trucks:
+    print "Alright, let's just take the trucks."
+else:
+    print "Fine, let's stay home then."
+
