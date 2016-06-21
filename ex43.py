ab33934a9176ed25439a2e157f81aed2734f71fa
superheroCEO <ja@bringbackfit.com>
2016-06-21 15:13:35 -0700

    adding a snippet of exercise 43

---
+++ ex43.py
@@ -0,0 +1,11 @@
+# OOP: objet oriented programming
+This process is just one way to do it that you can follow.
+
+The process is as follows:
+
+Write or draw about the problem.
+Extract key concepts from 1 and research them.
+Create a class hierarchy and object map for the concepts.
+Code the classes and a test to run them.
+Repeat and refine.
+
