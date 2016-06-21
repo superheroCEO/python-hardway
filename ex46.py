ac13a73a1bfb588011c95837b7f3b36507203303
superheroCEO <ja@bringbackfit.com>
2016-06-21 15:25:24 -0700

    adding some of exercise 46

---
+++ ex46.py
@@ -0,0 +1,12 @@
+Installing Python Packages
+Before you can begin this exercise you need to install some software for Python by using a tool called "pip" to install new modules. Here's the problem though. You are at a point where it's difficult for me to help you do that and keep this book sane and clean. There are so many ways to install software on so many computers that I'd have to spend 10 pages walking you through every step, and let me tell you I am a lazy guy.
+
+Rather than tell you how to do it exactly, I'm going to tell you what you should install, and then you tell you to watch the video that accompanies this exercise and follow along. You will have to struggle with this part of the exercise though as giving accurate and current installation instructions is tricky. Computers change frequently so you may have to do some research if you get stuck.
+
+Install the following Python packages:
+
+pip from http://pypi.python.org/pypi/pip
+distribute from http://pypi.python.org/pypi/distribute
+nose from http://pypi.python.org/pypi/nose/
+virtualenv from http://pypi.python.org/pypi/virtualenv
+
