97ee85fb8d87a0bfaff04286cc3f1fe9d880f037
superheroCEO <ja@bringbackfit.com>
2016-06-21 15:22:45 -0700

    adding some of exercise 45

---
+++ ex45.py
@@ -0,0 +1,8 @@
+Here are your requirements:
+
+Make a different game from the one I made.
+Use more than one file, and use import to use them. Make sure you know what that is.
+Use one class per room and give the classes names that fit their purpose (like GoldRoom, KoiPondRoom).
+Your runner will need to know about these rooms, so make a class that runs them and knows about them. There's plenty of ways to do this, but consider having each room return what room is next or setting a variable of what room is next.
+...some of the instructions...hmm....
+
