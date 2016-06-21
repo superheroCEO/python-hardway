a973c497baa650d09e861a478d8ad593d33c32c4
superheroCEO <ja@bringbackfit.com>
2016-06-21 16:09:06 -0700

    adding some of exercise 52

---
+++ ex52.py
@@ -0,0 +1,8 @@
+The Start Of Your Web Game
+We're coming to the end of the book, and in this exercise I'm going to really challenge you. When you're done, you'll be a reasonably competent Python beginner. You'll still need to go through a few more books and write a couple more projects, but you'll have the skills to complete them. The only thing in your way will be time, motivation, and resources.
+
+In this exercise, we won't make a complete game, but instead we'll make an "engine" that can run the game from Exercise 47 in the browser. This will involve refactoring Exercise 43, mixing in the structure from Exercise 47, adding automated tests, and finally creating a web engine that can run the games.
+
+This exercise will be huge, and I predict you could spend anywhere from a week to months on it before moving on. It's best to attack it in little chunks and do a bit a night, taking your time to make everything work before moving on.
+
+
