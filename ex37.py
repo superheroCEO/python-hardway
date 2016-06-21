4f7df0f3c7e2d9565488177ebe2b547c8386530b
superheroCEO <ja@bringbackfit.com>
2016-06-21 14:27:28 -0700

    adding exercise 37

---
+++ ex37.py
@@ -0,0 +1,8 @@
+It's time to review the symbols and Python words you know and to try to pick up a few more for the next few lessons. I have written out all the Python symbols and keywords that are important to know.
+
+In this lesson take each keyword and first try to write out what it does from memory. Next, search online for it and see what it really does. This may be difficult because some of these are difficult to search for, but try anyway.
+
+If you get one of these wrong from memory, make an index card with the correct definition and try to "correct" your memory.
+
+Finally, use each of these in a small Python program, or as many as you can get done. The goal is to find out what the symbol does, make sure you got it right, correct it if you do not, then use it to lock it in.
+
