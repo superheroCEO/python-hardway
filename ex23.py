cf0d353a2bffcb894c984b5dcbbcb61b40e1c4ea
superheroCEO <ja@bringbackfit.com>
2016-06-21 12:49:55 -0700

    adding exercise 23

---
+++ ex23.py
@@ -0,0 +1,8 @@
+Here's what you do:
+
+Go to bitbucket.org, github.com, or gitorious.org with your favorite web browser and search for "python."
+Pick a random project and click on it.
+Click on the Source tab and browse through the list of files and directories until you find a .py file.
+Start at the top and read through the code, taking notes on what you think it does.
+If any symbols or strange words seem to interest you, write them down to research later.
+
